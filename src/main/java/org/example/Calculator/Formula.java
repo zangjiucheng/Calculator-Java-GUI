f49f07a49e5a81c3c4afffe4c23614c465e113d7
Jiucheng <zangjiucheng@gmail.com>
2023-06-08 04:57:04 +0900

    Fix relative File Dirs (06/08/23)

---
+++ src/main/java/org/example/Calculator/Formula.java
@@ -0,0 +1,114 @@
+package org.example.Calculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * This class create a formula object with function process the formula.
+ *
+ * @version 1.1
+ * @auther Jiucheng Zang
+ * @since 2022-12-20
+ */
+
+public class Formula {
+    private String formula;
+
+    public Formula() {
+        formula = "";
+    }
+
+    public Formula(String formula) {
+        this.formula = formula;
+    }
+
+    /**
+     * @return ArrayList<String>
+     */
+    private ArrayList<String> formula_pop() {
+        ArrayList<String> popformula = new ArrayList<String>();
+        String[] formula_space = this.formula.split(" ");
+        String new_String = "";
+        for (String x : formula_space) {
+            new_String += x;
+        }
+        String[] temp_formula = new_String.split("");
+        popformula.addAll(Arrays.asList(temp_formula));
+        return popformula;
+    }
+
+    /**
+     * @return ArrayList<String>
+     */
+    public ArrayList<String> formula_format() {
+        ArrayList<String> input_formula = formula_pop();
+        ArrayList<String> formula_format = new ArrayList<String>();
+        String temp_number = "";
+        boolean symbol_status = false;
+        int temp_number_chr = -1;
+        for (String x : input_formula) {
+            char chr = x.charAt(0);
+            int number_chr = (int) chr;
+            if (((number_chr >= 48) && (number_chr <= 57)) || (number_chr == 46)) {
+                symbol_status = false;
+                temp_number += (char) number_chr;
+            } else {
+                if ((symbol_status) && (number_chr != 40) && (number_chr != 41) && (temp_number_chr != 40)
+                        && (temp_number_chr != 41)) {
+                    throw new IllegalArgumentException("Can't be two symbol together");
+                }
+                temp_number_chr = number_chr;
+                symbol_status = true;
+                if (temp_number.length() != 0) {
+                    formula_format.add(temp_number);
+                }
+                temp_number = "";
+                formula_format.add(x);
+            }
+        }
+        if (!temp_number.equals("")) {
+            formula_format.add(temp_number);
+        }
+        return formula_format;
+    }
+
+    /**
+     * @return double
+     */
+    public double process() {
+        ArrayList<String> format = formula_format();
+        Operator ff1 = new Operator(format);
+        return ff1.process();
+    }
+
+    /**
+     * @return String
+     */
+    public String toString() {
+        return String.format("%s", formula_format());
+    }
+
+    /**
+     * @return String
+     */
+    public String getFormula() {
+        return this.formula;
+    }
+
+    /**
+     * @param formula
+     */
+    public void setFormula(String formula) {
+        this.formula = formula;
+    }
+
+    /**
+     * @param formula
+     * @return Formula
+     */
+    public Formula formula(String formula) {
+        setFormula(formula);
+        return this;
+    }
+
+}
---
+++ src/main/java/org/example/GUI/operationElement.java
@@ -0,0 +1,156 @@
+package org.example.GUI;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ * This class include operationElementEdit.
+ * 
+ * @author Jiucheng Zang
+ * @version 1.2
+ * @since 2023-06-07
+ * @see Calculator.Formula
+ */
+
+public class operationElement {
+
+    /**
+     * @param panel
+     */
+    public static void operationElementEdit(JPanel panel) {
+        panel.setLayout(new GridLayout(5, 4));
+        int numberIndex = 1;
+        for (int numebrOfGrid = 0; numebrOfGrid < 20; numebrOfGrid++) {
+            if ((numebrOfGrid % 4 != 3) && (numebrOfGrid > 3) && (numebrOfGrid < 16)) {
+                JPanel tempPanel = basicElementEdit();
+                JButton numberIndexGrid = new JButton(String.valueOf(numberIndex));
+                final int numberCount = numberIndex;
+                numberIndexGrid.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        inputString += String.valueOf(numberCount);
+                        displayElement.refreshData(inputString);
+                    }
+                });
+                numberIndexGrid.setFont(new Font("Arial", Font.PLAIN, 25));
+                tempPanel.add(numberIndexGrid);
+                tempPanel.setBackground(Color.green);
+                panel.add(tempPanel);
+                numberIndex++;
+            } else {
+
+                switch (numebrOfGrid) {
+                    case 0: // Clear
+                        panel.add(functionElementEdit("AC", new Clear()));
+                        break;
+
+                    case 1: // ( )
+                        JPanel tempPanel = basicElementEdit();
+                        JButton functionGrid;
+                        tempPanel.setLayout(new GridLayout(0, 2));
+                        functionGrid = new JButton("(");
+                        JButton functionGrid2 = new JButton(")");
+                        functionGrid.addActionListener(new ActionListener() {
+                            public void actionPerformed(ActionEvent e) {
+                                inputString += "(";
+                                displayElement.refreshData(inputString);
+                            }
+                        });
+                        functionGrid2.addActionListener(new ActionListener() {
+                            public void actionPerformed(ActionEvent e) {
+                                inputString += ")";
+                                displayElement.refreshData(inputString);
+                            }
+                        });
+                        functionGrid.setFont(new Font("Arial", Font.PLAIN, 25));
+                        functionGrid2.setFont(new Font("Arial", Font.PLAIN, 25));
+                        tempPanel.add(functionGrid);
+                        tempPanel.add(functionGrid2);
+                        tempPanel.setBackground(Color.green);
+                        panel.add(tempPanel);
+                        break;
+
+                    case 2: // Backspace
+                        panel.add(functionElementEdit("⬅", new Backspace()));
+                        break;
+
+                    case 3: // divide
+                        panel.add(functionElementEdit("➗", new Divide()));
+                        break;
+
+                    case 7: // multiply
+                        panel.add(functionElementEdit("x", new Mutiple()));
+                        break;
+
+                    case 11: // minus
+                        panel.add(functionElementEdit("-", new Minus()));
+                        break;
+
+                    case 15: // plus
+                        panel.add(functionElementEdit("+", new Plus()));
+                        break;
+
+                    case 16: // number 0
+                        panel.add(functionElementEdit("0", new Zero()));
+                        break;
+
+                    case 17: // 10^n
+                        panel.add(functionElementEdit("10^n", new Multiplier()));
+                        break;
+
+                    case 18: // point
+                        panel.add(functionElementEdit(".", new Point()));
+                        break;
+
+                    case 19: // equal
+                        panel.add(functionElementEdit("=", new Equal()));
+                        break;
+
+                    default:
+                        JPanel BlackPanel = basicElementEdit();
+                        panel.add(BlackPanel);
+                        break;
+                }
+
+            }
+        }
+    }
+
+    /**
+     * @return JPanel
+     */
+    private static JPanel basicElementEdit() {
+        JPanel panel = new JPanel();
+        Utiles.transparentPanel(panel);
+        panel.setLayout(new GridLayout(0, 1));
+        return panel;
+
+    }
+
+    /**
+     * @param functionName
+     * @param function
+     * @return JPanel
+     */
+    private static JPanel functionElementEdit(String functionName, Function function) {
+        JPanel tempPanel = basicElementEdit();
+        JButton functionGrid;
+        functionGrid = new JButton(functionName);
+        functionGrid.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                function.run();
+            }
+        });
+        functionGrid.setFont(new Font("Arial", Font.PLAIN, 25));
+        tempPanel.add(functionGrid);
+        return tempPanel;
+    }
+
+    protected static String inputString = "";
+
+}
