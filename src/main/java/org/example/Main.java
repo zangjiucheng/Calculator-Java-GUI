7a97c7a9db494bdf7f0bd86ca1c7098f41a108fa
Jiucheng <zangjiucheng@gmail.com>
2023-06-08 04:45:25 +0900

    Gradle Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.GUI.mainGUI;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        mainGUI.setAbsPATH();
+        mainGUI.GUI();
+    }
+}
